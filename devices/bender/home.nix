de5855d5d68a99c534724eb9f5b1f33bd05d0369
Oscar Molnar <oscar@tymscar.com>
2024-04-09 23:58:39 +0100

    Set NZXT Kraken gif on Bender desktop at i3wm startup

--- devices/bender/home.nix
+++ devices/bender/home.nix
@@ -1,13 +1,29 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-let blenderWithCuda = pkgs.blender.override { cudaSupport = true; };
+let
+  blenderWithCuda = pkgs.blender.override { cudaSupport = true; };
+  aioImage = builtins.fetchurl {
+    url = "https://media.tenor.com/GIYnmPBTFsUAAAAM/keyboard-anime.gif";
+    sha256 = "sha256:1l16fz0zk5g79dbd0r9mj5qflj4fv60hl640ybc9icbvx9926j4w";
+  };
+  i3wm-module = import ../../apps/home-manager/i3wm {
+    inherit pkgs;
+    inherit lib;
+    inherit config;
+    additionalStartupCommands = [{
+      command =
+        "sudo ${pkgs.liquidctl}/bin/liquidctl -m nzxt set lcd screen gif ${aioImage}";
+      always = true;
+      notification = false;
+    }];
+  };
 in {
   imports = [
     ../../common/home.nix
+    i3wm-module
     ../../apps/home-manager/alacritty
     ../../apps/home-manager/zsh
     ../../apps/home-manager/pcmanfm
-    ../../apps/home-manager/i3wm
     ../../apps/home-manager/rofi
     ../../apps/home-manager/polybar
     ../../apps/home-manager/git
