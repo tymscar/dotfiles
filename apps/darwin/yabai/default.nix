f9d5c1e25e806985f8b9a20e06ffc79a38da3c8e
Oscar Molnar <oscar@tymscar.com>
2024-10-03 06:04:57 +0100

    Use my own fork for yabai (to bump to 7.1.4)

--- apps/darwin/yabai/default.nix
+++ apps/darwin/yabai/default.nix
@@ -1,8 +1,22 @@
 { pkgs, ... }:
-
-{
+let
+  myNixpkgs = pkgs.fetchFromGitHub {
+  owner = "tymscar";
+  repo = "nixpkgs";
+  rev = "yabaiHead";
+  sha256 = "sha256-nHdTda+unoMmjfuTSXKkBe672BNMsLNjNOjitwMcwJM=";
+};
+  myOverlay = final: prev: {
+    yabai = final.callPackage (myNixpkgs + "/pkgs/os-specific/darwin/yabai") {};
+  };
+  myPkgs = import pkgs.path { 
+    inherit (pkgs) system;
+    overlays = [ myOverlay ];
+  };
+in {
   services.yabai = {
     enable = true;
     extraConfig = builtins.readFile ./yabairc;
+    package = myPkgs.yabai;
   };
 }
