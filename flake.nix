93d171e368802d93a48ef513ed1ad656010d0016
Oscar Molnar <oscar@tymscar.com>
2025-11-19 23:05:04 +0000

    Update and remove temp fish overlay

--- flake.nix
+++ flake.nix
@@ -107,15 +107,6 @@
                       };
                       postInstall = ":";
                     });
-                    fish =
-                      builtins.trace
-                        "⚠️  WARNING: Fish tests are disabled due to macOS build failures (Nov 2024). Try removing the fish override in flake.nix:111-116 to check if it's fixed upstream."
-                        (
-                          prev.fish.overrideAttrs (oldAttrs: {
-                            doCheck = false;
-                            doInstallCheck = false;
-                          })
-                        );
                   })
                 ];
               }
