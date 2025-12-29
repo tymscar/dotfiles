7e00e824611f42aca6c329e8d2fa0a5482990385
Oscar Molnar <oscar@tymscar.com>
2025-12-29 22:54:19 +0000

    Move forgejo to local postgres and nas backup every day

--- apps/nixos/docker/forgejo/default.nix
+++ apps/nixos/docker/forgejo/default.nix
@@ -23,4 +23,40 @@ in
 
     wantedBy = [ "multi-user.target" ];
   };
+
+  systemd.services.forgejo-backup = {
+    description = "Backup Forgejo PostgreSQL database";
+    after = [
+      "forgejo.service"
+      "docker.service"
+      "mnt-nas.mount"
+    ];
+    requires = [
+      "forgejo.service"
+      "mnt-nas.mount"
+    ];
+    wants = [ "docker.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+      EnvironmentFile = [ docker-env ];
+    };
+    script = ''
+      ${pkgs.docker}/bin/docker exec forgejo-postgres \
+        pg_dump -U $POSTGRES_USER $POSTGRES_DB | \
+        ${pkgs.gzip}/bin/gzip > \
+        /mnt/nas/forgejo/pgBackup/forgejo-$(${pkgs.coreutils}/bin/date +%Y%m%d).sql.gz
+
+      cd /mnt/nas/forgejo/pgBackup
+      ls -t forgejo-*.sql.gz | tail -n +8 | xargs rm -f
+    '';
+  };
+
+  systemd.timers.forgejo-backup = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 03:00:00";
+      Persistent = true;
+    };
+  };
 }
