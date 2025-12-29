d885e2627198e12590b22e011931accbedc69cfb
Oscar Molnar <oscar@tymscar.com>
2025-12-29 23:02:13 +0000

    Move immich to local postgres and nas backup

--- apps/nixos/docker/immich/default.nix
+++ apps/nixos/docker/immich/default.nix
@@ -20,4 +20,40 @@ in
     };
     wantedBy = [ "multi-user.target" ];
   };
+
+  systemd.services.immich-backup = {
+    description = "Backup Immich PostgreSQL database";
+    after = [
+      "immich.service"
+      "docker.service"
+      "mnt-nas.mount"
+    ];
+    requires = [
+      "immich.service"
+      "mnt-nas.mount"
+    ];
+    wants = [ "docker.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+      EnvironmentFile = [ docker-env ];
+    };
+    script = ''
+      ${pkgs.docker}/bin/docker exec immich-postgres \
+        pg_dump -U postgres immich | \
+        ${pkgs.gzip}/bin/gzip > \
+        /mnt/nas/immich/pgBackup/immich-$(${pkgs.coreutils}/bin/date +%Y%m%d).sql.gz
+
+      cd /mnt/nas/immich/pgBackup
+      ls -t immich-*.sql.gz | tail -n +8 | xargs rm -f
+    '';
+  };
+
+  systemd.timers.immich-backup = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 03:00:00";
+      Persistent = true;
+    };
+  };
 }
