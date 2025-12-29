b8ce3f5e725b42f70f5531d6f838c74c03431284
Oscar Molnar <oscar@tymscar.com>
2025-12-29 23:02:51 +0000

    Move n8n to local postgres and nas backup

--- apps/nixos/docker/n8n/default.nix
+++ apps/nixos/docker/n8n/default.nix
@@ -23,4 +23,40 @@ in
 
     wantedBy = [ "multi-user.target" ];
   };
+
+  systemd.services.n8n-backup = {
+    description = "Backup n8n PostgreSQL database";
+    after = [
+      "n8n.service"
+      "docker.service"
+      "mnt-nas.mount"
+    ];
+    requires = [
+      "n8n.service"
+      "mnt-nas.mount"
+    ];
+    wants = [ "docker.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+      EnvironmentFile = [ dockerEnv ];
+    };
+    script = ''
+      ${pkgs.docker}/bin/docker exec n8n-postgres \
+        pg_dump -U $POSTGRES_USER $POSTGRES_DB | \
+        ${pkgs.gzip}/bin/gzip > \
+        /mnt/nas/n8n/pgBackup/n8n-$(${pkgs.coreutils}/bin/date +%Y%m%d).sql.gz
+
+      cd /mnt/nas/n8n/pgBackup
+      ls -t n8n-*.sql.gz | tail -n +8 | xargs rm -f
+    '';
+  };
+
+  systemd.timers.n8n-backup = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 03:00:00";
+      Persistent = true;
+    };
+  };
 }
