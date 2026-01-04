de3d9c3e2f9d19a057ccaf16636cdb73d88041c8
Oscar Molnar <oscar@tymscar.com>
2026-01-04 18:45:54 +0000

    Move traefik cert to local disk

--- apps/nixos/docker/traefik/default.nix
+++ apps/nixos/docker/traefik/default.nix
@@ -3,14 +3,17 @@ let
   docker-env = config.age.secrets.docker-traefik.path;
 in
 {
+  systemd.tmpfiles.rules = [
+    "d /var/lib/traefik 0700 root root -"
+    "f /var/lib/traefik/acme.json 0600 root root -"
+  ];
+
   systemd.services.traefik = {
     description = "Traefik";
     after = [
       "network.target"
       "docker.service"
-      "mnt-nas.mount"
     ];
-    requires = [ "mnt-nas.mount" ];
     wants = [ "docker.service" ];
     serviceConfig = {
       ExecStart = "${pkgs.docker}/bin/docker compose --env-file ${docker-env} -f docker-compose.yml up --force-recreate";
